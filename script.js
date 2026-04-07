57ddac9e2cda3d3b339c37d5b89bd7d4257e8ec0
Castro212010 <castro212010@gmail.com>
2026-04-07 11:26:41 +0530

    Nostra
    Upload codes

---
+++ script.js
@@ -0,0 +1,13 @@
+var sidenav=document.querySelector(".sidenav")
+
+function showsidenav()
+{
+   sidenav.style.display="block"
+}
+
+function closesidenav()
+{
+   sidenav.style.display="none"
+}
+
+
