bbd56e65ea970b126292c38b720513f43452469a
Castro212010 <castro212010@gmail.com>
2026-08-22 19:59:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,4 +11,23 @@ function closesidenav()
 }
 
 
+var productc=document.getElementById("product")
+var search=document.getElementById("search")
+var productl=productc.querySelectorAll("div")
+
+search.addEventListener("keyup",function(event){
+   var enterv=event.target.value.toUpperCase()
+   for(count=0;count<productl.length;count=count+1) {
+     var productn=productl[count].querySelector("p").textContent
+     if(productn.toUpperCase().indexOf(enterv)<0)
+   {
+     productl[count].style.display="none"
+   }
+   else {
+     productl[count].style.display="block"
+   }
+  }
+}
+)
+
 
